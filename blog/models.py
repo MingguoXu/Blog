210b77a8e7130efe9a35cd26515a8256a895a368
15696576570 <1741071488@qq.com>
2019-09-16 01:00:30 +0800

    中秋结束，学到详细页

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField()
-    created_time = models.DateField()
-    modified_time = models.DateField()
+    title = models.CharField('标题', max_length=100)
+    body = models.TextField('正文')
+    created_time = models.DateField('创建时间', default=timezone.now)
+    modified_time = models.DateField('修改时间')
     # 文章摘要
-    excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-# Create your models here.
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
